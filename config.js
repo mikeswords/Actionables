b554a084856f795c2a9d35b075b7c0e4c26295a1
Mike Swords <swords.mike@gmail.com>
2026-04-26 12:59:09 -0400

    chore: add Supabase config

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.ACTIONABLES_CONFIG = {
   appName: "Actionables",
-  supabaseUrl: "",
-  supabaseAnonKey: "",
+  supabaseUrl: "https://apqmameqqwwubjgxdnof.supabase.co",
+  supabaseAnonKey: "sb_publishable_tG8hf3NFF8-3RGEaPtpcfg_L0nhvqZU",
 };
